72525368cf08c8b8c6b41c2647848cad0058c439
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-10-10 00:44:08 -0300

    fix: remove console

--- js/modules/pagination.js
+++ js/modules/pagination.js
@@ -14,7 +14,6 @@ const currentPageFromLocalStorage = parseInt(localStorage.getItem("currentPage")
 const updateCurrentPage = (amountPageToChange, pageToGo) => {
   if (pageToGo) {
     currentPage = pageToGo;
-    console.log('entrou')
   } else {
     currentPage += amountPageToChange;
   }
