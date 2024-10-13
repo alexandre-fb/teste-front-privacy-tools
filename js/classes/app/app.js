e05b753f86a2f22732fe18f1d1f6336ef5fce2ee
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-10-13 16:48:09 -0300

    refactor: inicialization

--- js/classes/app/app.js
+++ js/classes/app/app.js
@@ -2,12 +2,14 @@ import ImageFetcher from "../imageFetcher/imageFetcher.js";
 import Pagination from "../pagination/pagination.js";
 import Drawer from "../drawer/drawer.js";
 import Zoom from "../zoom/zoom.js";
+
 class App {
   constructor() {
     this.imageFetcher = new ImageFetcher();
     this.pagination = new Pagination(this.imageFetcher);
     this.zoom = new Zoom();
-    this.drawer = new Drawer(this.zoom);
+    this.drawer = new Drawer(this.pagination, this.zoom);
+    this.imageFetcher.setDrawer(this.drawer);
     this.pageSavedLocalStorage = parseInt(
       localStorage.getItem("appCurrentPage")
     );
@@ -15,20 +17,27 @@ class App {
     this.init();
   }
 
-  init() {
+  async init() {
     this.pagination.init();
+    this.loadSavedPage();
+    await this.updateImageAndInitializeDrawer();
+  }
+
+  loadSavedPage() {
     if (this.pageSavedLocalStorage) {
       this.pagination.currentPage = this.pageSavedLocalStorage;
       this.pagination.validatePageRange();
       this.pagination.updateInputPage();
-      this.imageFetcher.fetchAndUpdateImage(this.pagination.currentPage);
-    } else {
-      this.imageFetcher.fetchAndUpdateImage(1);
     }
-    setTimeout(() => {
+  }
+
+  async updateImageAndInitializeDrawer() {
+    try {
+      await this.imageFetcher.fetchAndUpdateImage(this.pagination.currentPage);
       this.drawer.init();
-    }, 3000);
-    this.zoom.init();
+    } catch (error) {
+      console.error("Erro ao atualizar a imagem:", error);
+    }
   }
 }
 
