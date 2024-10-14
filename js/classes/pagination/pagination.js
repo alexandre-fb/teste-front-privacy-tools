b2adadbc09d82ef1b4d45d19cc559587a42e9467
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-10-14 02:16:57 -0300

    refactor

--- js/classes/pagination/pagination.js
+++ js/classes/pagination/pagination.js
@@ -1,7 +1,12 @@
 class Pagination {
   constructor(imageFetcher) {
-    this.paginationButtons = document.querySelectorAll('[data-button="pagination"]');
-    this.inputCurrentPage = document.querySelector('[data-input="current-page"]');
+    this.paginationButtons = document.querySelectorAll(
+      '[data-button="pagination"]'
+    );
+    this.inputCurrentPage = document.querySelector(
+      '[data-input="current-page"]'
+    );
+    this.totalPageLabel = document.querySelector('[data-input="total-page"]');
     this.currentPage = 1;
     this.totalPages = 100;
     this.imageFetcher = imageFetcher;
@@ -12,21 +17,30 @@ class Pagination {
       button.addEventListener("click", this.handleButtonClick);
     });
 
-    this.inputCurrentPage.addEventListener("change", this.handleInputPageChange);
+    this.inputCurrentPage.addEventListener(
+      "change",
+      this.handleInputPageChange
+    );
+    this.validatePageRange();
   }
 
+  setTotalPages = (totalPages) => {
+    this.totalPages = totalPages;
+    this.totalPageLabel.innerHTML = "/ " + this.totalPages;
+  };
+
   handleButtonClick = (event) => {
-    const valueToIncrement = event.target.dataset.buttonIncrement
-    this.incrementPage(valueToIncrement)
-  }
+    const valueToIncrement = event.target.dataset.buttonIncrement;
+    this.incrementPage(valueToIncrement);
+  };
 
   incrementPage = (amountToIncrement) => {
     this.currentPage += parseInt(amountToIncrement);
-    this.validatePageRange()
-    this.updateInputPage()
-    this.saveNewPageLocalStorage()
+    this.validatePageRange();
+    this.updateInputPage();
+    this.saveNewPageLocalStorage();
     this.imageFetcher.fetchAndUpdateImage(this.currentPage);
-  }
+  };
 
   validatePageRange = () => {
     if (this.currentPage < 1) {
@@ -34,22 +48,22 @@ class Pagination {
     } else if (this.currentPage > this.totalPages) {
       this.currentPage = this.totalPages;
     }
-  }
+  };
 
   handleInputPageChange = (event) => {
     this.currentPage = parseInt(event.target.value);
-    this.validatePageRange()
-    this.saveNewPageLocalStorage()
+    this.validatePageRange();
+    this.saveNewPageLocalStorage();
     this.imageFetcher.fetchAndUpdateImage(this.currentPage);
-  }
+  };
 
   updateInputPage = () => {
     this.inputCurrentPage.value = this.currentPage;
-  }
+  };
 
   saveNewPageLocalStorage = () => {
     localStorage.setItem("appCurrentPage", this.currentPage);
-  }
+  };
 }
 
 export default Pagination;
